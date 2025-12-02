fb3c30f0da9cea3c8d5eedf74b6fc659b109c337
Nikita <nikita@MacBookPro.fritz.box>
2025-12-02 15:07:45 +0100

    ready sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,168 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState } from 'react';
+import { FiCheck, FiX } from 'react-icons/fi';
+
+import styles from './page.module.css';
+
+type Question = {
+  key: string;
+  art: string;
+  plural: string;
+  trans: string;
+};
+type DataList = {
+  items: Question[];
+};
+
+const shuffle = (array: Question[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // random index 0..i
+    [array[i], array[j]] = [array[j], array[i]]; // swap
+  }
+  return array;
+};
 
 export default function Home() {
+  const [started, setStarted] = useState<boolean>(false);
+  const [end, setEnded] = useState<boolean>(false);
+  const [chosenAnswer, setChosenAnswer] = useState<number | null>(null);
+  const [currentKey, setCurrentKey] = useState<number>(0);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [answers, setAnswers] = useState<boolean[]>([]);
+
+  const startGame = () => {
+    fetch('/de.json')
+      .then((response) => response.json())
+      .then((data: DataList) => setQuestions(shuffle(data.items)))
+      .catch((error) => console.error('Error fetching data:', error));
+    setStarted(true);
+    setEnded(false);
+    setAnswers([]);
+    setCurrentKey(0);
+  };
+  const startAgain = () => {
+    fetch('/de.json')
+      .then((response) => response.json())
+      .then((data: DataList) => setQuestions(shuffle(data.items)))
+      .catch((error) => console.error('Error fetching data:', error));
+    setStarted(true);
+    setEnded(false);
+    setAnswers([]);
+    setCurrentKey(0);
+  };
+  const handleAnswer = (value: number, correct: boolean) => {
+    if (chosenAnswer !== null) {
+      return; // prevent multiple clicks
+    }
+    nextQuestion(value, correct);
+  };
+  const nextQuestion = (answer: number, correct: boolean) => {
+    setChosenAnswer(answer);
+    setAnswers((prevItems) => [...prevItems, correct]);
+    setTimeout(() => {
+      setChosenAnswer(null);
+      if (questions.length > currentKey + 1) {
+        setCurrentKey(currentKey + 1);
+        return;
+      }
+      setEnded(true);
+    }, 800);
+  };
+
+  const buttonFunc = (
+    chosenAnswer: number | null,
+    correctArt: string,
+    art: string,
+    value: number
+  ) =>
+    chosenAnswer === null || chosenAnswer !== value ? (
+      art
+    ) : chosenAnswer === value && correctArt === art ? (
+      <FiCheck />
+    ) : (
+      <FiX />
+    );
+
+  // console.log(questions);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        {started && end && (
+          <>
+            <div className={styles.starttitle}>The End</div>
+            <div className={styles.progress}>
+              {answers.map((i: boolean, index: number) => (
+                <span
+                  key={index}
+                  className={i ? styles.correct : styles.incorrect}
+                ></span>
+              ))}
+            </div>
+            <div className={styles.start} onClick={() => startAgain()}>
+              Start Again
+            </div>
+          </>
+        )}
+        {/* <span>started: {started ? 'true' : 'false'}</span>
+        <span>ended: {end ? 'true' : 'false'}</span>
+        <span>q: {questions.length}</span>
+        <span>key: {currentKey}</span> */}
+        {!started && !end && (
+          <>
+            <div className={styles.starttitle}>Start a Game ?</div>
+            <div className={styles.start} onClick={() => startGame()}>
+              Start
+            </div>
+          </>
+        )}
+        {started && !end && questions.length < 1 && (
+          <>
+            <div>No Questions</div>
+          </>
+        )}
+        {started && !end && questions.length > 0 && (
+          <div>
+            <div className={styles.progress}>
+              {answers.map((i: boolean, index: number) => (
+                <span
+                  key={index}
+                  className={i ? styles.correct : styles.incorrect}
+                ></span>
+              ))}
+            </div>
+            <div className={styles.question}>
+              {questions[currentKey].key}
+              <span>{questions[currentKey].trans}</span>
+            </div>
+            <div className={styles.buttons}>
+              <span
+                className={styles.fam}
+                onClick={() =>
+                  handleAnswer(1, questions[currentKey].art === 'die')
+                }
+              >
+                {buttonFunc(chosenAnswer, questions[currentKey].art, 'die', 1)}
+              </span>
+              <span
+                className={styles.uomo}
+                onClick={() =>
+                  handleAnswer(-1, questions[currentKey].art === 'der')
+                }
+              >
+                {buttonFunc(chosenAnswer, questions[currentKey].art, 'der', -1)}
+              </span>
+              <span
+                className={styles.neut}
+                onClick={() =>
+                  handleAnswer(0, questions[currentKey].art === 'das')
+                }
+              >
+                {buttonFunc(chosenAnswer, questions[currentKey].art, 'das', 0)}
+              </span>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
